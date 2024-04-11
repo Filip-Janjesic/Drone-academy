767d5ed8288b1bbed68af1c23b4bef901701fec9
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-11 17:27:16 +0200

    Create OznakaController.cs

---
+++ CSHARP/DroneAcademy/Controllers/OznakaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drone_academy.Controllers
+{
+    public class OznakaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
