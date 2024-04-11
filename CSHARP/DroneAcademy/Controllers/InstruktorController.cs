8d4b5d6fc372fe7739ae6656c8c83cde20f5591e
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-11 17:27:19 +0200

    Create InstruktorController.cs

---
+++ CSHARP/DroneAcademy/Controllers/InstruktorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drone_academy.Controllers
+{
+    public class InstruktorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
