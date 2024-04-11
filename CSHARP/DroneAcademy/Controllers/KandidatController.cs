e6f28533be010b62ea189aa4d2a65f49b1869207
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-11 17:27:14 +0200

    Create KandidatController.cs

---
+++ CSHARP/DroneAcademy/Controllers/KandidatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drone_academy.Controllers
+{
+    public class KandidatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
