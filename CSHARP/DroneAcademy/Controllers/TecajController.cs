a0091ab57e66b2bf750eb50e0de4b1a932e8a401
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-11 17:27:24 +0200

    Create TecajController.cs

---
+++ CSHARP/DroneAcademy/Controllers/TecajController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drone_academy.Controllers
+{
+    public class TecajController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
