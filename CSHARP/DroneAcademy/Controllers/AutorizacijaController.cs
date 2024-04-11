4f948e1c9d832967b0c366d204b140b841fa2951
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-11 17:27:10 +0200

    Create AutorizacijaController.cs

---
+++ CSHARP/DroneAcademy/Controllers/AutorizacijaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drone_academy.Controllers
+{
+    public class AutorizacijaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
