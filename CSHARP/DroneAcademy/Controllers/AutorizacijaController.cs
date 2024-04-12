10fa51eff25b004bf924435587f02b5d0c9c9f9a
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 00:52:46 +0200

    Update AutorizacijaController.cs

--- CSHARP/DroneAcademy/Controllers/AutorizacijaController.cs
+++ CSHARP/DroneAcademy/Controllers/AutorizacijaController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Drone_academy.Data;
+using Drone_academy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Drone_academy.Controllers
 {
-    public class AutorizacijaController : Controller
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AutorizacijaController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly DroneAcademyContext _context;
+
+        public AutorizacijaController(DroneAcademyContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpPost("token")]
+        public IActionResult GenerirajToken(OperaterDTO operater)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var operBaza = _context.Operateri
+                   .Where(p => p.Email!.Equals(operater.email))
+                   .FirstOrDefault();
+
+            if (operBaza == null)
+            {
+
+                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, ne mogu naći operatera");
+            }
+
+
+
+            if (!BCrypt.Net.BCrypt.Verify(operater.password, operBaza.Lozinka))
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, lozinka ne odgovara");
+            }
+
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno dugačak da se može koristiti");
+
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(8)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+
+            return Ok(jwt);
+
+
         }
     }
 }
