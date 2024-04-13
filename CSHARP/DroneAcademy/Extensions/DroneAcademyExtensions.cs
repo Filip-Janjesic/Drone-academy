e05e5bab37ff5c0c8bf6b0a9ce4b4724d405a4a7
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 22:40:40 +0200

    Update DroneAcademyExtensions.cs

--- CSHARP/DroneAcademy/Extensions/DroneAcademyExtensions.cs
+++ CSHARP/DroneAcademy/Extensions/DroneAcademyExtensions.cs
@@ -1,6 +1,86 @@
-﻿namespace Drone_academy.Extensions
+﻿using Drone_academy.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+
+namespace Drone_academy.Extensions
 {
-    public class DroneAcademyExtensions
+    public static class DroneAcademyExtensions
     {
+
+        public static void AddEdunovaSwaggerGen(this IServiceCollection Services)
+        {
+
+            Services.AddSwaggerGen(sgo =>
+            {
+                var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Drone Academy",
+                    Version = "v1",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "filip.janjesic@gmail.com",
+                        Name = "Filip Janješić"
+                    },
+                    Description = "Drone Academy",
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                    {
+                        Name = "Pilotska licenca"
+                    }
+                };
+                sgo.SwaggerDoc("v1", o);
+
+
+
+                sgo.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Autorizacija radi tako da se prvo na ruti /api/v1/Autorizacija/token.  
+                      autorizirate i dobijete token (bez navodnika). Upišite 'Bearer' [razmak] i dobiveni token.
+                      Primjer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2OTc3MTc2MjksImV4cCI6MTY5Nzc0NjQyOSwiaWF0IjoxNjk3NzE3NjI5fQ.PN7YPayllTrWESc6mdyp3XCQ1wp3FfDLZmka6_dAJsY'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                sgo.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+            });
+
+        }
+
+        public static void AddEdunovaCORS(this IServiceCollection Services)
+        {
+
+            Services.AddCors(opcije =>
+            {
+                opcije.AddPolicy("CorsPolicy",
+                    builder =>
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+
+            });
+
+        }
     }
 }
