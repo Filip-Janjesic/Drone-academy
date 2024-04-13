e81e6f75d9cc2696ad6b416a505520b705c3e2b2
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 13:53:05 +0200

    Update Operater.cs

--- CSHARP/DroneAcademy/Models/Operater.cs
+++ CSHARP/DroneAcademy/Models/Operater.cs
@@ -1,6 +1,11 @@
-﻿namespace Drone_academy.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drone_academy.Models
 {
-    public class Operater
+    public class Operater : Entitet
     {
+        public string? Email { get; set; }
+        public string? Lozinka { get; set; }
     }
 }
