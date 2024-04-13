27bc36c4dd2d1a18787a1865489329c867091a03
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 13:52:52 +0200

    Update Grupa.cs

--- CSHARP/DroneAcademy/Models/Grupa.cs
+++ CSHARP/DroneAcademy/Models/Grupa.cs
@@ -1,6 +1,21 @@
-﻿namespace Drone_academy.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drone_academy.Models
 {
-    public class Grupa
+    public class Grupa:Entitet
     {
+        public string? Naziv { get; set; }
+
+        [ForeignKey("instruktor")] // ovo pod navodnicima je naziv kolone u tablici grupa
+        public Instruktor? Instruktor { get; set; }
+
+        [ForeignKey("tecaj")]
+        public Tecaj? Tecaj { get; set; }
+
+        public int? MaksimalnoKandidata { get; set; }
+
+        public DateTime? DatumPocetka { get; set; }
+
+        public List<Kandidat>? Polaznici { get; set; }
     }
 }
