6fde2bd09cf2a1bd6602df7405c292ebd6672818
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 13:52:58 +0200

    Update Tecaj.cs

--- CSHARP/DroneAcademy/Models/Tecaj.cs
+++ CSHARP/DroneAcademy/Models/Tecaj.cs
@@ -1,6 +1,14 @@
-﻿namespace Drone_academy.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drone_academy.Models
 {
-    public class Tecaj
+    public class Tecaj:Entitet
     {
+        public string? Naziv { get; set; }
+        [Column("brojsati")]
+        public int? Trajanje { get; set; }
+        public decimal? Cijena { get; set; }
+        public decimal? Upisnina { get; set; }
     }
 }
