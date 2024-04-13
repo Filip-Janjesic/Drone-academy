04280f441760c7bfff0b19b2faf7d03e09f46a05
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 13:53:07 +0200

    Update Kandidat.cs

--- CSHARP/DroneAcademy/Models/Kandidat.cs
+++ CSHARP/DroneAcademy/Models/Kandidat.cs
@@ -1,6 +1,8 @@
 ﻿namespace Drone_academy.Models
 {
-    public class Kandidat
+    public class Kandidat:Osoba
     {
+        public string? BrojUgovora { get; set; }
+        public ICollection<Grupa>? Grupe { get; } = new List<Grupa>();
     }
 }
