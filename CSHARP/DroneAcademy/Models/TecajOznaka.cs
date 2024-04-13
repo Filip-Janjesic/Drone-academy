7ea1c4fd41e3ce10aacfebffd09acd5e6af06ca3
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 13:52:56 +0200

    Update TecajOznaka.cs

--- CSHARP/DroneAcademy/Models/TecajOznaka.cs
+++ CSHARP/DroneAcademy/Models/TecajOznaka.cs
@@ -1,6 +1,15 @@
-﻿namespace Drone_academy.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drone_academy.Models
 {
-    public class TecajOznaka
+    public class TecajOznaka:Entitet
     {
+        [ForeignKey("tecaj")]
+        public Tecaj? Tecaj { get; set; }
+        [ForeignKey("oznaka")]
+        public Oznaka? Oznaka { get; set; }
+        public string? napomena { get; set; }
+
     }
 }
