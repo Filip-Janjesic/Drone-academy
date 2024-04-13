312c37b9d906a49febe7c530736134acfcc9128d
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 23:17:31 +0200

    Update MappingKandidat.cs

--- CSHARP/DroneAcademy/Mappers/MappingKandidat.cs
+++ CSHARP/DroneAcademy/Mappers/MappingKandidat.cs
@@ -1,6 +1,61 @@
-﻿namespace Drone_academy.Mappers
+﻿using AutoMapper;
+using Drone_academy.Models;
+
+namespace Drone_academy.Mappers
 {
-    public class MappingKandidat
+    public class MappingKandidat : Mapping<Kandidat, KandidatDTORead, KandidatDTOInsertUpdate>
     {
+
+        public MappingKandidat()
+        {
+            MapperMapReadToDTO = new Mapper(
+            new MapperConfiguration(c =>
+            {
+                c.CreateMap<Kandidat, KandidatDTORead>()
+                .ConstructUsing(entitet =>
+                 new KandidatDTORead(
+                    entitet.Sifra,
+                    entitet.Ime,
+                    entitet.Prezime,
+                    entitet.Email,
+                    entitet.Oib,
+                    entitet.BrojUgovora,
+                    PutanjaDatoteke(entitet)));
+            })
+            );
+
+            MapperMapInsertUpdateToDTO = new Mapper(
+              new MapperConfiguration(c =>
+              {
+                  c.CreateMap<Kandidat, KandidatDTOInsertUpdate>()
+               .ConstructUsing(entitet =>
+                new KandidatDTOInsertUpdate(
+                   entitet.Ime,
+                   entitet.Prezime,
+                   entitet.Email,
+                   entitet.Oib,
+                   entitet.BrojUgovora,
+                   PutanjaDatoteke(entitet)));
+              })
+              );
+        }
+
+
+        private static string PutanjaDatoteke(Kandidat e)
+        {
+            try
+            {
+                var ds = Path.DirectorySeparatorChar;
+                string slika = Path.Combine(Directory.GetCurrentDirectory()
+                    + ds + "wwwroot" + ds + "slike" + ds + "kandidati" + ds + e.Sifra + ".png");
+                return File.Exists(slika) ? "/slike/kandidati/" + e.Sifra + ".png" : null;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
     }
 }
