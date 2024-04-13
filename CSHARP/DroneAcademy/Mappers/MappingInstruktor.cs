1e57a90060031c222643e1cdad04f2322fd7587d
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-13 23:14:58 +0200

    Update MappingInstruktor.cs

--- CSHARP/DroneAcademy/Mappers/MappingInstruktor.cs
+++ CSHARP/DroneAcademy/Mappers/MappingInstruktor.cs
@@ -1,6 +1,52 @@
-﻿namespace Drone_academy.Mappers
+﻿using AutoMapper;
+using Drone_academy.Models;
+
+namespace Drone_academy.Mappers
 {
-    public class MappingInstruktor
+    public class MappingInstruktor : Mapping<Instruktor, InstruktorDTORead, InstruktorDTOInsertUpdate>
     {
+
+        public MappingInstruktor()
+        {
+            MapperMapReadToDTO = new Mapper(
+            new MapperConfiguration(c =>
+            {
+                c.CreateMap<Instruktor, InstruktorDTORead>()
+                .ConstructUsing(entitet =>
+                 new InstruktorDTORead(
+                    entitet.Sifra,
+                    entitet.Ime,
+                    entitet.Prezime,
+                    entitet.Email,
+                    entitet.Oib,
+                    entitet.Iban,
+                    PutanjaDatoteke(entitet)));
+            })
+            );
+        }
+
+        private static string PutanjaDatoteke(Instruktor e)
+        {
+            try
+            {
+                var ds = Path.DirectorySeparatorChar;
+                string dir = Path.Combine(Directory.GetCurrentDirectory()
+                    + ds + "wwwroot" + ds + "datoteke" + ds + "instruktori" + ds);
+                DirectoryInfo d = new DirectoryInfo(dir);
+                FileInfo[] Files = d.GetFiles(e.Sifra + "_*");
+                return Files != null && Files.Length > 0 ? "/datoteke/instruktori/" + Files[0].Name : null;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+
+
+
+
+
     }
 }
