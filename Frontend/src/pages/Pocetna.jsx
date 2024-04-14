dc8490196f12b658fc48becaef3fe3c1393e98a4
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-14 16:12:50 +0200

    Create Pocetna.jsx

---
+++ Frontend/src/pages/Pocetna.jsx
@@ -0,0 +1,9 @@
+import { Container } from 'react-bootstrap';
+
+export default function Pocetna() {
+  return (
+    <Container>
+      <p>Dobro došli na drone akademiju!</p>
+    </Container>
+  );
+}
