707b29dd1fe687ff10dfbec7c3bbb56cef7a4870
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-14 14:16:03 +0200

    Create InstruktorService.js

---
+++ Frontend/src/services/InstruktorService.js
@@ -0,0 +1,7 @@
+import  { get,obrisi,dodaj,getBySifra,promjeni, httpService, obradiUspjeh, obradiGresku } from "./httpService";
+
+async function postaviDatoteku(sifra,datoteka,config) {
+    return await httpService.patch('/Instruktor/' + sifra,datoteka,config).then((res)=>{return obradiUspjeh(res);}).catch((e)=>{ return obradiGresku(e);});
+  }
+
+export default{get,obrisi,dodaj,getBySifra,promjeni,postaviDatoteku};
