4e76009fb570dfd286679906af594621b04abebc
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-14 14:02:50 +0200

    Create constants.js

---
+++ Frontend/src/constants.js
@@ -0,0 +1,31 @@
+export const RoutesNames = {
+    HOME: '/',
+
+    NADZORNA_PLOCA: '/nadzornaploca',
+
+    TECAJEVI_PREGLED: '/tecajevi',
+    TECAJEVI_NOVI: '/tecajevi/dodaj',
+    TECAJEVI_PROMJENI: '/tecajevi/:sifra',
+
+    INSTRUKTORI_PREGLED: '/instruktori',
+    INSTRUKTORI_NOVI: '/instruktori/dodaj',
+    INSTRUKTORI_PROMJENI: '/instruktori/:sifra',
+
+
+    KANDIDATI_PREGLED: '/kandidati',
+    KANDIDATI_NOVI: '/kandidati/dodaj',
+    KANDIDATI_PROMJENI: '/kandidati/:sifra',
+
+    GRUPE_PREGLED: '/grupe',
+    GRUPE_NOVI: '/grupe/dodaj',
+    GRUPE_PROMJENI: '/grupe/:sifra',
+
+    OZNAKE_PREGLED: '/oznake',
+
+    LOGIN: '/login'
+};
+
+export const App = {
+    URL: '',
+    DEV: false
+};
