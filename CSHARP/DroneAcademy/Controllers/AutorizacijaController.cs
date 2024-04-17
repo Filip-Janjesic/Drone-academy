e56abd136b055f603e75364fafc25c042818e919
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-17 13:31:21 +0200

    Update AutorizacijaController.cs

--- CSHARP/DroneAcademy/Controllers/AutorizacijaController.cs
+++ CSHARP/DroneAcademy/Controllers/AutorizacijaController.cs
@@ -1,9 +1,6 @@
 ﻿using Drone_academy.Data;
 using Drone_academy.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
 
 namespace Drone_academy.Controllers
 {
@@ -21,47 +18,8 @@ namespace Drone_academy.Controllers
         [HttpPost("token")]
         public IActionResult GenerirajToken(OperaterDTO operater)
         {
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var operBaza = _context.Operateri
-                   .Where(p => p.Email!.Equals(operater.email))
-                   .FirstOrDefault();
-
-            if (operBaza == null)
-            {
-
-                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, ne mogu naći operatera");
-            }
-
-
-
-            if (!BCrypt.Net.BCrypt.Verify(operater.password, operBaza.Lozinka))
-            {
-                return StatusCode(StatusCodes.Status403Forbidden, "Niste autorizirani, lozinka ne odgovara");
-            }
-
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno dugačak da se može koristiti");
-
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(8)),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var jwt = tokenHandler.WriteToken(token);
-
-
-            return Ok(jwt);
-
-
+            // If you decide to keep this method, remove the authorization logic and return a different response
+            return Ok("Authorization logic removed. Modify this method as needed.");
         }
     }
 }
