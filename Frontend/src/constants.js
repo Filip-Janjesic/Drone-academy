be4bb0059d1d4d7514cce8d38558d71d10c95623
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-18 18:57:30 +0200

    fix

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -26,6 +26,6 @@ export const RoutesNames = {
 };
 
 export const App = {
-    URL: 'http://filipjanjesic1-001-site1.anytempurl.com',
+    URL: 'https://filipjanjesic1-001-site1.anytempurl.com',
     DEV: true
 };
